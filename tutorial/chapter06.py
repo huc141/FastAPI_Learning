35e18d2624a4be8adf810d0ddf3994314d57eeb8
yulong <luoyl@reolink.com.cn>
2023-04-18 00:30:58 +0800

    第六章第二节

--- tutorial/chapter06.py
+++ tutorial/chapter06.py
@@ -1,3 +1,103 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordRequestForm, OAuth2PasswordBearer
+from pydantic import BaseModel
+from typing import Optional
 
 app06 = APIRouter()
+
+"""OAuth2密码模式和FastAPI的OAuth2PasswordBearer"""
+
+"""
+OAuth2PasswordBearer是接收URL作为参数的一个类：客户端会向该URL发送username和password参数，然后得到一个token值
+OAuth2PasswordBearer并不会创建相应的URL路径操作，只是指明客户端用来请求Token的URL地址
+当请求到来的时候，FASTAPI会检查请求的Authorization头信息，如果没有找到Authorization头信息，
+或者头信息的内容不是Bearer token，它会返回401状态码（unauthorized）
+"""
+
+oauth2_schema = OAuth2PasswordBearer(tokenUrl="/chatpter06/token") # 请求token的URL地址 http://127.0.0.1:8000/chapter06/token
+
+@app06.get("/oauth2_password_bearer")
+async def oauth2_password_bearer(token: str = Depends(oauth2_schema)):
+    return {"token": token}
+
+
+"""基于password和bearer token的oauth2 认证"""
+
+# 用列表来模拟一个存储了账户和密码的数据库
+fake_users_db = {
+    "johnsnow": {
+        "username": "johnsnow",
+        "full_name": "JohnSnow",
+        "email": "johnsnow@example.com",
+        "hashed_password": "fakehashedsecret",
+        "disabled": False,
+    },
+    "alice": {
+        "username": "alice",
+        "full_name": "Alice Wonderson",
+        "email": "alice@example.com",
+        "hashed_password": "fakehashedsecret2",
+        "disabled": True,
+    },
+}
+
+# 模拟对密码的hash加密
+def fake_hash_password(password: str):
+    return "fakehashed" + password
+
+
+class User(BaseModel):
+    username: str
+    email: Optional[str] = None
+    full_name: Optional[str] = None
+    disabled: Optional[bool] = None
+
+
+class UserInDB(User):
+    hashed_password: str
+
+
+def get_user(db, username: str):
+    if username in db:
+        user_dict = db[username]
+        return UserInDB(**user_dict)
+
+
+# 模拟对获取的token进行解码校验
+def fake_decode_token(token: str):
+    user = get_user(fake_users_db, token)
+    return user
+
+
+async def get_current_user(token: str = Depends(oauth2_schema)):
+    user = fake_decode_token(token)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="无效的授权认证！",
+            headers={"WWW-Authenticate": "Bearer"}, # OAuth2的规范，如果认证失败，请求头中返回“Authenticate”
+        )
+    return user
+
+
+async def get_current_active_user(current_user: User = Depends(get_current_user)):
+    if current_user.disabled:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="当前用户未激活！")
+    return current_user
+
+
+@app06.post("/token")
+async def login(form_data: OAuth2PasswordRequestForm = Depends()):
+    user_dict = fake_users_db.get(form_data.username)
+    if not user_dict:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="无效的用户名或密码！")
+    user = UserInDB(**user_dict)
+    hashed_password = fake_hash_password(form_data.password)
+    if not hashed_password == user.hashed_password:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="无效的用户名或密码！")
+    return {"access_token": user.username, "token_type": "bearer"}
+
+
+@app06.get("/users/me")
+async def read_users_me(current_user: User = Depends(get_current_active_user)):
+    return current_user
